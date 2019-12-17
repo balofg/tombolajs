bc2df558b4deccf219afe54f87b71fdc8a798b97
Barnaba Longo <blongo@cuebiq.com>
2019-12-17 13:00:01 +0100

    save to local storage and add reset option

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cell from "./Cell";
 import { toggleNumber } from "../state/numbers";
 import Popup from "./Popup";
@@ -9,6 +9,23 @@ const Table = () => {
   const [selectedNumbers, setSelectedNumbers] = useState([]);
   const [popupNumber, setPopupNumber] = useState(null);
   const [isSmorfiaEnabled, setIsSmorfiaEnabled] = useState(true);
+
+  useEffect(() => {
+    let preloadedSelectedNumbers;
+
+    try {
+      preloadedSelectedNumbers = JSON.parse(
+        localStorage.getItem("tombola") || "[]"
+      );
+    } catch (error) {
+      preloadedSelectedNumbers = [];
+    } finally {
+      setSelectedNumbers(preloadedSelectedNumbers);
+    }
+
+    if (preloadedSelectedNumbers) {
+    }
+  }, []);
 
   const cells = Array(90)
     .fill(null)
@@ -21,6 +38,22 @@ const Table = () => {
   const rows = Array(3)
     .fill(null)
     .map((item, index) => index);
+
+  const handleReset = () => {
+    localStorage.removeItem("tombola");
+    setSelectedNumbers([]);
+  };
+
+  const handleNumberClick = number => {
+    if (isSmorfiaEnabled && selectedNumbers.indexOf(number) === -1) {
+      setPopupNumber(number);
+    }
+
+    const newSelectedNumbers = toggleNumber(number, selectedNumbers);
+
+    setSelectedNumbers(toggleNumber(number, selectedNumbers));
+    localStorage.setItem("tombola", JSON.stringify(newSelectedNumbers));
+  };
 
   return (
     <div className="table">
@@ -35,16 +68,7 @@ const Table = () => {
                     number={cell}
                     key={cell}
                     selected={selectedNumbers.indexOf(cell) > -1}
-                    onClick={number => {
-                      if (
-                        isSmorfiaEnabled &&
-                        selectedNumbers.indexOf(number) === -1
-                      ) {
-                        setPopupNumber(number);
-                      }
-
-                      setSelectedNumbers(toggleNumber(number, selectedNumbers));
-                    }}
+                    onClick={handleNumberClick}
                   />
                 ))}
             </div>
@@ -56,8 +80,9 @@ const Table = () => {
       ) : null}
 
       <div className="settings">
+        <h3 className="settings-title">Settings</h3>
         <ul className="settings-list">
-          <li>
+          <li className="settings-item">
             Smorfia:{" "}
             <a
               className={`settings-action ${
@@ -75,6 +100,14 @@ const Table = () => {
               onClick={() => setIsSmorfiaEnabled(false)}
             >
               OFF
+            </a>
+          </li>
+          <li className="settings-item">
+            <a
+              className="settings-action settings-action--active"
+              onClick={handleReset}
+            >
+              RESET
             </a>
           </li>
         </ul>
